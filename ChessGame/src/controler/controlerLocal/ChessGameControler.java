f7029d64b21c5d106a628bbda776b95c2b992be3
thestranger26 <thestranger26@hotmail.fr>
2015-11-02 11:46:16 +0100

    Implementation de la classe ChessGameControler

--- ChessGame/src/controler/controlerLocal/ChessGameControler.java
+++ ChessGame/src/controler/controlerLocal/ChessGameControler.java
@@ -4,10 +4,40 @@
  */
 package controler.controlerLocal;
 
+import model.Coord;
+import model.Couleur;
+import controler.ChessGameControlers;
+import model.observable.ChessGame;
+
 /**
  *
  * @author Tristan
  */
-public class ChessGameControler {
+public class ChessGameControler implements ChessGameControlers{
+
+    ChessGame chessGame;
+
+    public ChessGameControler(ChessGame chessGame) {
+        this.chessGame = chessGame;
+    }
     
+    @Override
+    public boolean move(Coord initCoord, Coord finalCoord) {
+       return chessGame.move(initCoord.x, initCoord.y, finalCoord.x, finalCoord.y);
+    }
+
+    @Override
+    public String getMessage() {
+        return chessGame.getMessage();
+    }
+
+    @Override
+    public boolean isEnd() {
+        return chessGame.isEnd();
+    }
+
+    @Override
+    public Couleur getColorCurrentPlayer() {
+        return chessGame.getColorCurrentPlayer();
+    }
 }
